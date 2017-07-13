9c7b5bd19ed0f8d6750500f9bee9fec277bbb798
Giuliana Marquesi <giugianam@gmail.com>
2017-07-13 14:27:44 -0300

    Criadas respostas

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-var vagina = null;
+var listaResposta = [
+  'Vai catar coquinho!',
+  'Se joga miga!',
+  'Sei lá'
+]
 
 function carregaConselho() {
-  vagina = document.getElementById('vagina');
+
+  var conselhoP = document.createElement('p');
+  var conselhoDiv = document.createElement('div');
+  var vagina = document.getElementById('vagina');
+  var conteiner = document.getElementById('conteiner-da-vagina');
+
+  var numero = Math.floor(Math.random() * listaResposta.length);
+
   vagina.className = 'carregando-conselho';
-  console.log(vagina);
-  slee
-  //vagina.className = 'vagina';
+
+  conselhoP.innerHTML = listaResposta[numero];
+  console.log(numero);
+  console.log(listaResposta);
+
+  conselhoDiv.classList.add("resposta-balao");
+  conselhoP.classList.add("resposta-texto");
+  conselhoDiv.appendChild(conselhoP);
+  conteiner.appendChild(conselhoDiv);
+
+
 }
