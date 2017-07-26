7c3d00b7b1fd6a10b7d72775445d430f224ed17e
Giuliana Marquesi <giugianam@gmail.com>
2017-07-26 14:21:48 -0300

    animação melhorada!

--- main.js
+++ main.js
@@ -4,17 +4,38 @@ var listaResposta = [
   'Sei lá'
 ]
 
+var conselhoP = document.getElementById('resposta-texto');
+var conselhoDiv = document.getElementById('resposta-balao');
+
 function carregaConselho() {
 
+  var vagina = document.getElementById('vagina');
+
+  vagina.classList.add('carregando-conselho');
+
+  vagina.addEventListener("animationend", falaConselho);
+  vagina.addEventListener("animationstart", someConselho);
+
+
+}
+
+function falaConselho() {
   var conselhoP = document.getElementById('resposta-texto');
   var conselhoDiv = document.getElementById('resposta-balao');
   var vagina = document.getElementById('vagina');
 
-  var numero = Math.floor(Math.random() * listaResposta.length);
-
-  vagina.classList.remove('carregando-conselho');
-  vagina.classList.add('carregando-conselho');
-
   conselhoDiv.style.display = 'block';
+  conselhoP.style.display = 'block';
+  vagina.classList.remove('carregando-conselho');
+}
+
+function someConselho() {
+  var conselhoP = document.getElementById('resposta-texto');
+  var conselhoDiv = document.getElementById('resposta-balao');
+
+  conselhoP.style.display = 'none';
+  conselhoDiv.style.display = 'none';
+
+  var numero = Math.floor(Math.random() * listaResposta.length);
   conselhoP.innerHTML = listaResposta[numero];
 }
