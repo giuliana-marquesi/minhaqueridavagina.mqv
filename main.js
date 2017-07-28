cfa4de8cfb205f366a6e9a4e9004efbb8c2c5ebb
Giuliana Marquesi <giugianam@gmail.com>
2017-07-28 01:07:11 -0300

    numero como global, e frases nao aleatorias

--- main.js
+++ main.js
@@ -17,24 +17,28 @@ var listaResposta = [
 ]
 
 var cont = 0;
+var numero = 0;
 
 function carregaConselho() {
 
   var conselhoP = document.getElementById('resposta-texto');
   var conselhoDiv = document.getElementById('resposta-balao');
   var vagina = document.getElementById('vagina');
-  var numero = Math.floor(Math.random() * listaResposta.length);
+  numero = Math.floor(Math.random() * listaResposta.length);
 
   vagina.classList.add('carregando-conselho');
 
-  vagina.addEventListener("animationend", function() {falaConselho(vagina, conselhoDiv, conselhoP, numero)});
+  vagina.addEventListener("animationend", function() {falaConselho(vagina, conselhoDiv, conselhoP, cont)});
   vagina.addEventListener("animationstart", function() {someConselho(conselhoDiv, conselhoP)});
-  console.log("entrou no carregaConselho");
+  if (cont < listaResposta.length - 1){
+    cont++;
+  } else {
+    cont = 0;
+  }
 
 }
 
 function falaConselho(vagina, conselhoDiv, conselhoP, numero) {
-  console.log("entrou no fala conselho");
   conselhoDiv.style.display = 'block';
   conselhoP.style.display = 'block';
   vagina.classList.remove('carregando-conselho');
